7afee2e2af17beceef42884fc6544d87bb2c2f05
himanshuji7777 <59680926+himanshuji7777@users.noreply.github.com>
2021-07-19 18:12:03 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "ENV" {} 
+variable "DEPLOY_ROLE" {} 
+variable "WEBAPP_DNS" {} 
+variable "WEBAPP_CERT" {} 
+variable "WEB_ACL_ID_1" {} 
+variable "WEB_ACL_ID_2" {}
+variable "ROUTE53_HOSTED_ZONE_NAME" {}
